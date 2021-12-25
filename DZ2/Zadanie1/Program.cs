743b383de0ee72c2943a4b6ec5c00c5d173d536d
Ислам <G_islam05@mail.ru>
2021-12-25 12:55:37 +0300

    DZ2.Zadanie1

---
+++ DZ2/Zadanie1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Zadanie1;
+
+
+SchetVBanke moySchet = new SchetVBanke();
+moySchet.Balans = 800.93m;
+moySchet.TypeScheta = type.Budgetny;
+moySchet.Number = "5469600147901901";
+
+Console.WriteLine($"Номер счета: {moySchet.Number} \n Тип счета: {moySchet.TypeScheta} \n Баланс: {moySchet.Balans}");
---
+++ DZ2/Zadanie1/SchetVBanke.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    enum type
+    {
+        Budgetny,
+        Valutny,
+        Zamorojenny,
+        Zastraxovanny,
+    }
+
+    internal class SchetVBanke
+    {
+        private string number;
+        private decimal balans;
+        private type typeScheta;
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                this.number = value;
+            }
+        }
+        public decimal Balans
+        {
+            get { return balans; }
+            set { this.balans = value; }
+        }
+        public type TypeScheta
+        {
+            get { return typeScheta; }
+            set { this.typeScheta = value;}
+        }
+    }
+}
