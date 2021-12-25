8ce851ee2017dec1878ee2565a5860dfa9059fd5
Ислам <G_islam05@mail.ru>
2021-12-25 13:42:13 +0300

    DZ2.Zadanie1

--- DZ2/Zadanie1/Program.cs
+++ DZ2/Zadanie1/Program.cs
@@ -6,5 +6,5 @@ SchetVBanke moySchet = new SchetVBanke();
 moySchet.Balans = 800.93m;
 moySchet.TypeScheta = type.Budgetny;
 moySchet.Number = "5469600147901901";
-
+Console.WriteLine();
 Console.WriteLine($"Номер счета: {moySchet.Number} \n Тип счета: {moySchet.TypeScheta} \n Баланс: {moySchet.Balans}");
