a66d05953e6fc3fd050f7050369618a5af0d0a74
Ислам <G_islam05@mail.ru>
2021-12-26 16:33:51 +0300

    DZ3.Zadanie2

---
+++ DZ3/Zadanie2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите строку для переворота");
+
+Console.WriteLine(Reverse(Console.ReadLine()));
+string Reverse(string s)
+{
+    char[] a = s.ToCharArray();
+    Array.Reverse(a);
+    
+    return String.Concat(a);
+}
