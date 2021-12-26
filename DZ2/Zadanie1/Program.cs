ea55889afd6a19e6c2a7a56c9b424edb18b4d8ce
Ислам <G_islam05@mail.ru>
2021-12-26 16:49:38 +0300

    DZ3.Zadanie2

--- DZ2/Zadanie1/Program.cs
+++ DZ2/Zadanie1/Program.cs
@@ -1,15 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Zadanie1;
+Console.WriteLine("Введите строку для переворота");
 
+Console.WriteLine(Reverse(Console.ReadLine()));
+string Reverse(string s)
+{
+    char[] a = s.ToCharArray();
+    Array.Reverse(a);
 
-SchetVBanke moySchet = new SchetVBanke(1000, type.Zamorojenny);
-SchetVBanke moySchet2 = new SchetVBanke(300, type.Budgetny);
-
-moySchet.snyat(60);
-Console.WriteLine($"Состояние до перевода: Счет 1 = {moySchet.Balans} Счет 2 = {moySchet2.Balans}");
-moySchet.perevod(moySchet2, 200);
-Console.WriteLine($"Результат после перевода:  Счет 1 = {moySchet.Balans} Счет 2 = {moySchet2.Balans}");
-Console.WriteLine($"Остаток: {moySchet.Balans}");
-
-moySchet.polojit(500);
-Console.WriteLine($"Остаток: {moySchet.Balans}");
+    return String.Concat(a);
+}
