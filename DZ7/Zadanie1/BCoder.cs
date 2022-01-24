2f730efb123ec18a3237440813ec828a8d60cff4
Ислам <G_islam05@mail.ru>
2022-01-24 21:00:45 +0300

    Готово

--- DZ7/Zadanie1/BCoder.cs
+++ DZ7/Zadanie1/BCoder.cs
@@ -3,31 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Zadanie1;
 
 namespace Zadanie1
 {
-    internal class ACoder : ICoder
+    internal class BCoder:ICoder
     {
         public string Encode(string str)
         {
-            string str1="";
+            int begin = (int)'а';
+            int end = (int)'я';
+            string str1 = "";
             foreach (char a in str)
             {
                 if (a == 'я') str1 += 'а';
-                else str1 += Convert.ToChar (a+1);
+                else str1 += Convert.ToChar(Math.Abs (end - (a-begin)));
             }
             return str1;
-            
+
         }
 
         public string Decode(string str)
         {
+            int begin = (int)'я';
+            int end = (int)'а';
             string str1 = "";
             foreach (char a in str)
             {
                 if (a == 'а') str1 += 'я';
-                else str1 += Convert.ToChar(a - 1);
+                else str1 += Convert.ToChar(Math.Abs(end - (a - begin)));
             }
             return str1;
         }
