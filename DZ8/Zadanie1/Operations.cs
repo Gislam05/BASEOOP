545b94811a7d8dc4d645e592cac8da8f80e298e9
Ислам <G_islam05@mail.ru>
2022-02-04 19:38:38 +0300

    Пока только это..

---
+++ DZ8/Zadanie1/Operations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    internal interface Operations
+    {
+        
+        
+       
+        public void Rename();
+        public void Copy();
+        public void Move();        
+        public double Size();
+        
+        public void Info();
+        
+    }
+}
